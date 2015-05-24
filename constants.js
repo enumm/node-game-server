ea88dcf2c09e033596bfc9e4c55c9de6940765d4
enumm <weliukis@gmail.com>
2015-05-24 23:58:15 +0300

    smth :sob:

--- constants.js
+++ constants.js
@@ -22,13 +22,13 @@ exports.DataBase = {
 };
 
 exports.Races = {
-	Plebs: {name :'Plebs', buildings: ['PlebHut', 'PlebRanger', 'PlebFlying']},
-	BlaBlas: {name :'BlaBlas', buildings: ['BlaHut', 'BlaRanger', 'BlaFlying']}
+    Plebs: {name :'Plebs', buildings: ['PlebHut','PlebHut2', 'PlebRanger', 'PlebRanger2', 'PlebFlying', 'PlebFlying2']},
+    BlaBlas: {name :'BlaBlas', buildings: ['BlaHut','BlaHut2', 'BlaRanger','BlaRanger2', 'BlaFlying','BlaFlying2']}
 };
 
 exports.UnitTypes = {
-	Pleb: {name: 'Pleb', type: 'ground', damage: 10, armor: 10, movementSpeed: 60, life: 100, attackSpeed: 1, range: 15},
-	RangedPleb: {name: 'RangedPleb', type: 'ranged', damage: 8, armor: 5, movementSpeed: 60, life: 100, attackSpeed: 2, range: 120},
+    Pleb: {name: 'Pleb', type: 'ground', damage: 10, armor: 10, movementSpeed: 60, life: 100, attackSpeed: 1, range: 15},
+    RangedPleb: {name: 'RangedPleb', type: 'ranged', damage: 8, armor: 5, movementSpeed: 60, life: 100, attackSpeed: 2, range: 120},
     FlyingPleb: {name: 'FlyingPleb', type: 'flying', damage: 5, armor: 4, movementSpeed: 60, life: 100, attackSpeed: 3, range: 35},
 
 
@@ -38,11 +38,17 @@ exports.UnitTypes = {
 };
 
 exports.BuildingTypes = {
-    PlebHut : {name: 'PlebHut', cost: 2, frame: 2, life: 200, armor: 20, buildTime: 10, unitType: 'Pleb'},
+    PlebHut : {name: 'PlebHut', cost: 1, frame: 2, life: 200, armor: 20, buildTime: 10, unitType: 'Pleb'},
+    PlebHut2 : {name: 'PlebHut2', cost: 1, frame: 2, life: 200, armor: 20, buildTime: 10, unitType: 'Pleb'},
     PlebRanger : {name: 'PlebRanger', cost: 5, frame: 3, life: 200, armor: 20, buildTime: 10, unitType: 'RangedPleb'},
+    PlebRanger2 : {name: 'PlebRanger2', cost: 5, frame: 3, life: 200, armor: 20, buildTime: 10, unitType: 'RangedPleb'},
     PlebFlying : {name: 'PlebFlying', cost: 10, frame: 4, life: 200, armor: 20, buildTime: 10, unitType: 'FlyingPleb'},
+    PlebFlying2 : {name: 'PlebFlying2', cost: 10, frame: 4, life: 200, armor: 20, buildTime: 10, unitType: 'FlyingPleb'},
 
-    BlaHut : {name: 'BlaHut', cost: 2, frame: 2, life: 200, armor: 20, buildTime: 10, unitType: 'Bla'},
+    BlaHut : {name: 'BlaHut', cost: 1, frame: 2, life: 200, armor: 20, buildTime: 10, unitType: 'Bla'},
+    BlaHut2 : {name: 'BlaHut2', cost: 1, frame: 2, life: 200, armor: 20, buildTime: 10, unitType: 'Bla'},
     BlaRanger : {name: 'BlaRanger', cost: 5, frame: 3, life: 200, armor: 20, buildTime: 10, unitType: 'RangedBla'},
-    BlaFlying : {name: 'BlaFlying', cost: 10, frame: 4, life: 200, armor: 20, buildTime: 10, unitType: 'FlyingBla'}
+    BlaRanger2 : {name: 'BlaRanger2', cost: 5, frame: 3, life: 200, armor: 20, buildTime: 10, unitType: 'RangedBla'},
+    BlaFlying : {name: 'BlaFlying', cost: 10, frame: 4, life: 200, armor: 20, buildTime: 10, unitType: 'FlyingBla'},
+    BlaFlying2 : {name: 'BlaFlying2', cost: 10, frame: 4, life: 200, armor: 20, buildTime: 10, unitType: 'FlyingBla'}
 };
